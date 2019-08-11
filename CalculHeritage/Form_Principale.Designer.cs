78f0e4e0d9fb9885a18c0404bb231b393b27a7f1
Dopamax <heisenberg.57.yolo@gmail.com>
2019-08-11 02:09:34 +0100

    Fusion de l'interface Form principale

--- CalculHeritage/Form_Principale.Designer.cs
+++ CalculHeritage/Form_Principale.Designer.cs
@@ -206,6 +206,12 @@
             this.Controls.Add(this.question1CU1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.bunifuFlatButton1);
+            this.Name = "Form_Principale";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1215, 544);
+            this.MinimumSize = new System.Drawing.Size(1215, 544);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calcul d\'Héritage";
